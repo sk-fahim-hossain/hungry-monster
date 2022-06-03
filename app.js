8220741d378a883bb63efc4ae1bb4cd7e901a150
sk-fahim-hossain <fahimhossainjob1@gmail.com>
2022-06-03 12:41:20 +0600

    this is the final project

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+// load foods after loading
+
 fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
 .then(res => res.json())
 .then(data =>{
@@ -5,17 +8,56 @@ fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
     displayFood(food)
 })
 
-document.getElementById("search-btn").addEventListener("click",function(){
+
+// single food cards
+const displayFood = food => {
+    {
+        food.map(fd=>{
+            const foodDisplay = document.getElementById("searchedFood")
+            const foodGroupDiv = document.createElement('div')
+            foodGroupDiv.className = "food-group";
+            const foodInfo = `
+               
+                <a href="#food-details" style="text-decoration: none;">
+                    <div onclick="foodCardDetail('${fd.strMeal}')" class="card food-card" style="width: 18rem;">
+                        <img src="${fd.strMealThumb}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${fd.strMeal}</h5>
+                        </div>
+                    </div>
+                </a>
+            `
+            
+            foodGroupDiv.innerHTML = foodInfo;
+            foodDisplay.appendChild(foodGroupDiv);
+           
+        })
+    }
+}
+
+// search input
+document.getElementById("search-btn").addEventListener("click",function(e){
+    e.preventDefault();
     const inputName = document.getElementById("search-input").value;
+    console.log(inputName)
     foodCardDetail(inputName)
 })
 
-function foodCardDetail(mealName){
-   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+
+// food card details
+
+async function foodCardDetail(inputName){
+    console.log('click')
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputName}`)
    .then(res => res.json())
    .then(data =>{
         const meal = data.meals[0];
-        const foodCardDisplay = document.getElementById("foodDetails")
+        singleFoodCard(meal)
+   })  
+}
+
+function singleFoodCard(meal){
+    const foodCardDisplay = document.getElementById("foodDetails")
         const singleCardDiv = document.createElement('div')
         singleCardDiv.addClass = "singleCardDiv"
         let singleCard = `
@@ -42,33 +84,5 @@ function foodCardDetail(mealName){
         `
         singleCardDiv.innerHTML = singleCard;
         foodCardDisplay.appendChild(singleCardDiv)
-   })  
 }
-
-const displayFood = food => {
-    {
-        food.map(fd=>{
-            const foodDisplay = document.getElementById("searchedFood")
-            const foodGroupDiv = document.createElement('div')
-            foodGroupDiv.className = "food-group";
-            const foodInfo = `
-               
-                <a href="#food-details" style="text-decoration: none;">
-                    <div onclick="foodCardDetail('${fd.strMeal}')" class="card food-card" style="width: 18rem;">
-                        <img src="${fd.strMealThumb}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${fd.strMeal}</h5>
-                        </div>
-                    </div>
-                </a>
-            `
-            
-            foodGroupDiv.innerHTML = foodInfo;
-            foodDisplay.appendChild(foodGroupDiv);
-           
-        })
-    }
-}
-
-
 
